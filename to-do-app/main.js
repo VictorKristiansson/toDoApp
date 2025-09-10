85574f33be86ebb118a432e0fa1e20cc49be3057
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-09-10 14:32:31 +0700

    Update main.js
    added a task remaining counter

--- to-do-app/main.js
+++ to-do-app/main.js
@@ -3,86 +3,86 @@ let writeTaskInput = document.getElementById("write-task");
 let taskList = document.getElementById("task-list");
 let errorMsg = document.getElementById("error-msg");
 let taskText = document.getElementById("task-text");
+let taskCounterElement = document.getElementById("task-remaining-counter");
+
 
 
 // UPDATE INSTRUCTION TEXT
 const updateInstructions = () => {
   if (taskList.children.length === 0) {
     taskText.textContent = "Type below to add a task";
-  }
-
-  else {
+  } else {
     taskText.textContent = "Add another task?";
   }
 };
+
 
 
 // MARK TASK AS DONE
 const markTaskAsDone = (li) => {
   li.remove();
   updateInstructions();
-}
+  taskCounterFunction();
+};
+
+
 
 // DELETE TASK
 const deleteTask = (li) => {
   li.remove();
   updateInstructions();
-}
-
-// TASK COUNTER
-const taskCounterFunction = (taskList) => {
-  let taskCounter = taskList.children.length;
-}
-
-
-
-
-//ADD TASK
-addTaskButton.onclick = () => {
-
-errorMsg.textContent = "";
-
-  if (writeTaskInput.value.trim() === "") {
-    errorMsg.textContent = "Your task can't be empty!"
-  }
-
-  else if (writeTaskInput.value.length > 50) {
-    errorMsg.textContent = "Your task can't longer than 50 characters!"
-  }
-
-  else {
-  let li = document.createElement("li");
-
-  let span = document.createElement("span");
-  span.textContent = writeTaskInput.value;
-
-
-  let markAsCompleteBtn = document.createElement("button");
-  markAsCompleteBtn.textContent = "✔";
-  markAsCompleteBtn.onclick = () => {
-  deleteTask(li);
-  };
-  
- 
-  
-
-  let deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "✖";
-  deleteBtn.onclick = () => {
-  deleteTask(li);
-  };
-  
-  li.appendChild(span);
-  li.appendChild(markAsCompleteBtn);
-  li.appendChild(deleteBtn);
-
-  taskList.appendChild(li);
-  writeTaskInput.value = "";
-  updateInstructions();
-}
-
+  taskCounterFunction();
 };
 
+
+
+// TASK COUNTER
+const taskCounterFunction = () => {
+  let taskCount = taskList.children.length;
+  taskCounterElement.textContent = `You currently have ${taskCount} remaining tasks!`;
+};
+
+
+
+// ADD TASK
+addTaskButton.onclick = () => {
+  errorMsg.textContent = "";
+
+  if (writeTaskInput.value.trim() === "") {
+    errorMsg.textContent = "Your task can't be empty!";
+  } else if (writeTaskInput.value.length > 50) {
+    errorMsg.textContent = "Your task can't longer than 50 characters!";
+  } else {
+    let li = document.createElement("li");
+
+    let span = document.createElement("span");
+    span.textContent = writeTaskInput.value;
+
+    let markAsCompleteBtn = document.createElement("button");
+    markAsCompleteBtn.textContent = "✔";
+    markAsCompleteBtn.onclick = () => {
+      markTaskAsDone(li);
+    };
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "✖";
+    deleteBtn.onclick = () => {
+      deleteTask(li);
+    };
+
+    li.appendChild(span);
+    li.appendChild(markAsCompleteBtn);
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+    writeTaskInput.value = "";
+    updateInstructions();
+    taskCounterFunction();
+  }
+};
+
+
+
+// RESET INSTRUCTIONS AND TASK COUNTER
 updateInstructions();
-
-
+taskCounterFunction();
